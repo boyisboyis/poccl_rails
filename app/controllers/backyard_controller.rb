377a1797e4badd8a906dac1f0aa184564e90a69d
Supagrid Tangsermsit <boyisboyis261129@gmail.com>
2015-05-22 07:20:19 +0000

    Update

--- app/controllers/backyard_controller.rb
+++ app/controllers/backyard_controller.rb
@@ -1,5 +1,17 @@
 class BackyardController < ApplicationController
+    before_action :require_login
+    
     def index 
         
     end
+    def require_login
+     flash[:notice] = nil
+     if current_user.blank?
+     	redirect_to login_path
+     	#render  '/frontend/account/login', layout: false
+       #redirect_to backyard_login_path
+     #elsif !current_user.blank? 
+       #redirect_to backyard_root_path
+     end
+   end
 end
